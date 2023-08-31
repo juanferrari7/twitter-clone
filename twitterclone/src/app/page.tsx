c54e487a1491fd30e2581f9f9a08aed54e307932
Chimi <juan.ferrari304@gmail.com>
2023-08-31 17:42:45 -0300

    done

--- twitterclone/src/app/page.tsx
+++ twitterclone/src/app/page.tsx
@@ -1,48 +1,32 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
-import { AuthButtonServer } from './components/auth-button-server'
+import { AuthButtonServer } from '@/app/components/auth-button-server'
 import { redirect } from 'next/navigation'
-import PostCard from './components/post-card'
+import { PostLists } from './components/posts-list'
+import { type Database } from './types/database'
+import { ComposePost } from './components/compose-post'
 
 export default async function Home () {
-  const supabase = createServerComponentClient({ cookies })
+  const supabase = createServerComponentClient<Database>({ cookies })
   const { data: { session } } = await supabase.auth.getSession()
 
-  if (session === null) redirect('/login')
+  if (session === null) {
+    redirect('/login')
+  }
 
   const { data: posts } = await supabase
     .from('posts')
-    .select('*, user:users(name, user_name, avatar_url)')
+    .select('*, user:users(name, avatar_url, user_name)')
+    .order('created_at', { ascending: false })
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+
+      <section className="max-w-[600px] w-full mx-auto border-l border-r border-white/20 min-h-screen">
+        <ComposePost userAvatarUrl={session.user?.user_metadata?.avatar_url} />
+        <PostLists posts={posts} />
+      </section>
       <AuthButtonServer />
-
-      {
-        posts?.map(post => {
-          const {
-            id,
-            user,
-            content
-          } = post
-
-          const {
-            user_name: userName,
-            name: userFullName,
-            avatar_url: avatarUrl
-          } = user
-
-          return (
-            <PostCard
-              avatarUrl={avatarUrl}
-              content={content}
-              key={id}
-              userFullName={userFullName}
-              userName={userName}
-              />
-          )
-        })
-      }
     </main>
   )
 }
